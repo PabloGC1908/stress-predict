54e18a76f8a2798d3cf438666f4ceda377bde5c0
Pablo Guerra Camana <pguerracamana@gmail.com>
2025-04-27 10:48:16 -0500

    Añadido estructuras iniciales

---
+++ stress-predict/src/main/java/com/pgc/stress_predict/StressPredictApplication.java
@@ -0,0 +1,12 @@
+package com.pgc.stress_predict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StressPredictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StressPredictApplication.class, args);
+	}
+}
---
+++ stress-predict/src/test/java/com/pgc/stress_predict/StressPredictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgc.stress_predict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StressPredictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
