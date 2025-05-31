fef86356c03e0cfc8a32358d1d43c144046fc342
Pablo Guerra Camana <pguerracamana@gmail.com>
2025-05-31 00:12:15 -0500

    Terminado endpoint para predecir estres

--- backend/core-service/src/main/java/com/pgc/stress_predict/application/dto/request/EncuestaRequest.java
+++ backend/core-service/src/main/java/com/pgc/stress_predict/application/dto/request/EncuestaRequest.java
@@ -1,6 +1,12 @@
 package com.pgc.stress_predict.application.dto.request;
 
 public record EncuestaRequest(
-
+        Integer idUsuario,
+        Float horasEstudioDia,
+        Float horasExtracurricularDia,
+        Float horasSuenoDia,
+        Float horasSocialDia,
+        Float horasActividadFisica,
+        String comentario
 ) {
 }
