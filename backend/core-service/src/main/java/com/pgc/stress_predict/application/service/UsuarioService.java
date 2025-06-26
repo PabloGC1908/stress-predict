c60174a90dab0a686934a0ee4a8c11c33f6c3498
Pablo Guerra Camana <pguerracamana@gmail.com>
2025-06-25 23:14:49 -0500

    Endpoint de actualizacion de perfil terminado

--- backend/core-service/src/main/java/com/pgc/stress_predict/application/service/UsuarioService.java
+++ backend/core-service/src/main/java/com/pgc/stress_predict/application/service/UsuarioService.java
@@ -1,15 +1,21 @@
 package com.pgc.stress_predict.application.service;
 
+import com.pgc.stress_predict.application.dto.request.PerfilUsuarioUpdateRequest;
 import com.pgc.stress_predict.application.dto.response.HistorialUsuarioResponse;
 import com.pgc.stress_predict.application.dto.response.PerfilUsuarioResponse;
+import com.pgc.stress_predict.domain.model.Usuario;
+import com.pgc.stress_predict.domain.model.UsuarioInfo;
 import com.pgc.stress_predict.domain.port.out.UsuarioRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PatchMapping;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
 
@@ -43,5 +49,46 @@ public class UsuarioService {
             throw new UsernameNotFoundException("El id de usuario no se encontro");
         else
             return perfilUsuario.get();
+    }
+
+
+    public String updatePerfilUsuario(Long usuarioId, PerfilUsuarioUpdateRequest perfilUsuarioUpdateRequest) {
+        if (usuarioId == null)
+            throw new UsernameNotFoundException("El id de usuario no puede ser nulo");
+
+        if (perfilUsuarioUpdateRequest == null)
+            throw new UsernameNotFoundException("La perfil no puede ser nula");
+
+        Optional<Usuario> usuario = usuarioRepository.findById(usuarioId);
+
+        if (usuario.isEmpty()) {
+            throw new UsernameNotFoundException("El id de usuario no existe");
+        }
+        else {
+            UsuarioInfo usuarioInfo = usuario.get().getUsuarioInfo();
+            log.info("Datos del contacto: {}", usuarioInfo.toString());
+            log.info("Datos a actualizar: {}", perfilUsuarioUpdateRequest);
+
+            if (perfilUsuarioUpdateRequest.nombre() != null)
+                usuarioInfo.setNombre(perfilUsuarioUpdateRequest.nombre());
+
+            if (perfilUsuarioUpdateRequest.apellido() != null)
+                usuarioInfo.setApellido(perfilUsuarioUpdateRequest.apellido());
+
+            if (perfilUsuarioUpdateRequest.telefono() != null)
+                usuarioInfo.setTelefono(perfilUsuarioUpdateRequest.telefono());
+
+            if (perfilUsuarioUpdateRequest.dni() != null)
+                usuarioInfo.setDni(perfilUsuarioUpdateRequest.dni());
+
+            if (perfilUsuarioUpdateRequest.fechaNacimiento() != null)
+                usuarioInfo.setFechaNacimiento(perfilUsuarioUpdateRequest.fechaNacimiento());
+
+            usuario.get().setUsuarioInfo(usuarioInfo);
+
+            usuarioRepository.save(usuario.get());
+        }
+
+        return "Se actualizo el perfil de usuario correctamente";
     }
 }
